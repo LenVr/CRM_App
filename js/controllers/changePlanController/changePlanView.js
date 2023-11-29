a6db0115e914ad8009d37d0101f632783d51be2b
Len <villalobosrojas22@gmail.com>
2023-11-29 17:04:48 -0600

    save all changes

--- js/controllers/changePlanController/changePlanView.js
+++ js/controllers/changePlanController/changePlanView.js
@@ -30,35 +30,69 @@ export class ChangePlanView extends ViewForController {
         this.localStoragePlanName = localStorage.getItem('name');
         this.createOptionPlanInfo()
 
+        this.acceptBtn1 = div(this.optionPlanContainer1, { className: "changeBtn", innerHTML: "Select plan" });
+
+        this.acceptBtn2 = div(this.optionPlanContainer2, { className: "changeBtn", innerHTML: "Select plan" });
+
+
     }
 
     createOptionPlanInfo() {
         if (this.localStoragePlanName === 'Intermediate') {
+
             this.planName = h2(this.optionPlanContainer1, { className: "planName", innerHTML: "Plan: Advanced" });
-            this.planUserAmount = p(this.optionPlanContainer1, { className: 'planUserAmount', innerHTML: "Price: 4" });
+            this.planUserAmount = p(this.optionPlanContainer1, { className: 'planUserAmount', innerHTML: "User: 4" });
             this.planPrice = p(this.optionPlanContainer1, { className: "planPrice", innerHTML: `Price: $109.99` });
+            this.informationPlanContainer = div(this.optionPlanContainer1, { className: "planInformationContainer" })
+
+
+            this.planInformation = p(this.informationPlanContainer, { className: 'planInformation', innerHTML: "The group option for $109.99. Full access for up to four people, maximizing collaborative learning. Elevate your language skills together!" })
 
             this.planName = h2(this.optionPlanContainer2, { className: "planName", innerHTML: "Plan: Basic" });
-            this.planUserAmount = p(this.optionPlanContainer2, { className: 'planUserAmount', innerHTML: "Price: 1" });
+            this.planUserAmount = p(this.optionPlanContainer2, { className: 'planUserAmount', innerHTML: "User: 1" });
             this.planPrice = p(this.optionPlanContainer2, { className: "planPrice", innerHTML: `Price: $69.99` });
+            this.informationPlanContainer = div(this.optionPlanContainer2, { className: "planInformationContainer" })
+
+
+            this.planInformation = p(this.informationPlanContainer, { className: 'planInformation', innerHTML: "Experience personalized English learning for $69.99. Full access to exclusive classes and resources to enhance your skills at your own pace." })
+
         }
         if (this.localStoragePlanName === 'Advanced') {
+
             this.planName = h2(this.optionPlanContainer1, { className: "planName", innerHTML: "Plan: Intermediate" });
-            this.planUserAmount = p(this.optionPlanContainer1, { className: 'planUserAmount', innerHTML: "Price: 2" });
+            this.planUserAmount = p(this.optionPlanContainer1, { className: 'planUserAmount', innerHTML: "User: 2" });
             this.planPrice = p(this.optionPlanContainer1, { className: "planPrice", innerHTML: `Price: $84.99` });
+            this.informationPlanContainer = div(this.optionPlanContainer1, { className: "planInformationContainer" })
+
+
+            this.planInformation = p(this.informationPlanContainer, { className: 'planInformation', innerHTML: "Share the learning experience for $84.99. Premium access for two users, strengthening skills together." })
+
 
             this.planName = h2(this.optionPlanContainer2, { className: "planName", innerHTML: "Plan: Basic" });
-            this.planUserAmount = p(this.optionPlanContainer2, { className: 'planUserAmount', innerHTML: "Price: 1" });
+            this.planUserAmount = p(this.optionPlanContainer2, { className: 'planUserAmount', innerHTML: "User: 1" });
             this.planPrice = p(this.optionPlanContainer2, { className: "planPrice", innerHTML: `Price: $69.99` });
+            this.informationPlanContainer = div(this.optionPlanContainer2, { className: "planInformationContainer" })
+
+
+            this.planInformation = p(this.informationPlanContainer, { className: 'planInformation', innerHTML: "Experience personalized English learning for $69.99. Full access to exclusive classes and resources to enhance your skills at your own pace." })
         }
         if (this.localStoragePlanName === 'Basic') {
+
             this.planName = h2(this.optionPlanContainer1, { className: "planName", innerHTML: "Plan: Intermediate" });
-            this.planUserAmount = p(this.optionPlanContainer1, { className: 'planUserAmount', innerHTML: "Price: 2" });
+            this.planUserAmount = p(this.optionPlanContainer1, { className: 'planUserAmount', innerHTML: "User: 2" });
             this.planPrice = p(this.optionPlanContainer1, { className: "planPrice", innerHTML: `Price: $84.99` });
+            this.informationPlanContainer = div(this.optionPlanContainer1, { className: "planInformationContainer" })
+
+            this.planInformation = p(this.informationPlanContainer, { className: 'planInformation', innerHTML: "Share the learning experience for $84.99. Premium access for two users, strengthening skills together." })
+
 
             this.planName = h2(this.optionPlanContainer2, { className: "planName", innerHTML: "Plan: Advanced" });
-            this.planUserAmount = p(this.optionPlanContainer2, { className: 'planUserAmount', innerHTML: "Price: 4" });
+            this.planUserAmount = p(this.optionPlanContainer2, { className: 'planUserAmount', innerHTML: "User: 4" });
             this.planPrice = p(this.optionPlanContainer2, { className: "planPrice", innerHTML: `Price: $109.99` });
+            this.informationPlanContainer = div(this.optionPlanContainer2, { className: "planInformationContainer" })
+
+            this.planInformation = p(this.informationPlanContainer, { className: 'planInformation', innerHTML: "The group option for $109.99. Full access for up to four people, maximizing collaborative learning. Elevate your language skills together!" })
+
         }
     }
 
